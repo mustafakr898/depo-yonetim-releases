1771919cac56dc02661ca9a972a55240c3eb2567
Depo <donek0101@gmail.com>
2026-09-26 14:26:00 +0300

    web v0.9.75

--- depo_sw.js
+++ depo_sw.js
@@ -1,6 +1,6 @@
 // OTOMATIK URETILDI (web_sw_uret.ps1) â€” elle duzenleme.
 // Cevrimdisi acilis: uygulama dosyalari onbellekte; veri tarayici DB'sinde.
-const SURUM = 'depo-0.9.74-20260925152324';
+const SURUM = 'depo-0.9.75-20260926142556';
 const DOSYALAR = [
   './',
   'assets/AssetManifest.bin',
